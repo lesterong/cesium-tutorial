58c32bfb648d0bf242fef3e8036e402c64adb96b
Lester Ong <lesterong776@gmail.com>
2023-12-31 21:54:02 +0800

    Initialise repo with quickstart

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['plugin:astro/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  ignorePatterns: ['dist', 'node_modules'],
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+      rules: {},
+    },
+  ],
+};
---
+++ src/env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="astro/client" />
+interface Window {
+  CESIUM_BASE_URL: string;
+}
